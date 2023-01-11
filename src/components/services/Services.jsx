2c3cc61065331f207ae936b0bda21a4dcaaf0991
venkata2003 <yerrapati2003@gmail.com>
2023-01-11 21:59:01 +0530

    Second Commit

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+import './services.css'
+import {BiCheck} from 'react-icons/bi'
+const Services = () => {
+    return (
+        <section id="services">
+            <h5>What I Offer</h5>
+            <h2>Services</h2>
+            <div className="container services__container">
+                {/* UI/UX Design */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>UI/UX Design</h3>
+                    </div>
+
+                    <ul className="service__list">
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF UI/UX Design */}
+
+                {/* Web Development */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Web Development</h3>
+                    </div>
+
+                    <ul className="service__list">
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF WEb DEvelopment */}
+
+                {/* Content Creation */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Content Creation</h3>
+                    </div>
+
+                    <ul className="service__list">
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                        <li>
+                            <BiCheck className="service__list-icon" />
+                            <p>helklon the things are the things how the thigns ar egoing on the snfkdgkii</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* Content Creatoion */}
+            </div>
+        </section>
+    )
+}
+
+export default Services
